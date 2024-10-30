5b3011dc921bea4a0522e7baad16f119d8f655ce
siba02 <sibaprasad.tofan@gmail.com>
2024-10-30 11:43:51 +0530

    new file

---
+++ test.py
@@ -0,0 +1,28 @@
+# string and numeric value can operate with *.
+A,B=2,3
+txt="#"
+print(2*txt*3)
+
+# string and string can operate with +.
+X,Y="2",3
+Txt="@"
+print((X+Txt)*Y)
+
+# numeric value can operate with all arithmatic operators.
+O,P=5,6
+Q=7
+print(O+P*Q)
+
+# Arithmatic expression with integer and float will result in float.
+S=8.0
+T=6
+print(S*T)
+
+# floor gives closest integer which lesser than or equal to the float value.
+N=13
+M=4
+V=N//M
+print(V)
+
+# Reminder is negative when denominator is negative.
+print(12%-5)
